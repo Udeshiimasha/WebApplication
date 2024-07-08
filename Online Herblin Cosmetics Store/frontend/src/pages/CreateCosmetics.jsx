726fd6c3bb0c942c5caa0ac4a58c5a7d646bc189
Imasha Udeshi Senevirathna <139677721+Udeshiimasha@users.noreply.github.com>
2024-07-08 15:43:05 +0530

    Update CreateCosmetics.jsx

--- Online Herblin Cosmetics Store/frontend/src/pages/CreateCosmetics.jsx
+++ Online Herblin Cosmetics Store/frontend/src/pages/CreateCosmetics.jsx
@@ -4,6 +4,9 @@ import Spinner from '../components/Spinner';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { useSnackbar } from 'notistack';
+import css from "./Home.module.css";
+import img from '../Images/wallpaper4.jpg';
+import { Background } from 'react-parallax';
 
 const CreateCosmetics = () => {
   const [item, setItem] = useState('');
@@ -27,7 +30,7 @@ const CreateCosmetics = () => {
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Cosmetic Created successfully', { variant: 'success' });
-        navigate('/');
+        navigate('/home');
       })
       .catch((error) => {
         setLoading(false);
@@ -38,10 +41,10 @@ const CreateCosmetics = () => {
   };
 
   return (
-    
+    <div style={{ backgroundImage: `url(${img})`, backgroundSize: 'cover', backgroundPosition: 'center', height: '100vh' }}>
     <div className='p-4'>
       <BackButton />
-      <h1 className='text-3xl my-4'>Create Cosmetic</h1>
+      <h1 className='text-4xl my-4'>Create Cosmetic</h1>
       {loading ? <Spinner /> : ''}
       <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
         <div className='my-4'>
@@ -84,6 +87,7 @@ const CreateCosmetics = () => {
           Save
         </button>
       </div>
+    </div>
     </div>
   );
 }
